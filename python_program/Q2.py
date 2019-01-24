4737476cb1fb26d2f09ddfa29b631fab2e56a6d6
unknown <narendra_php@Narendraphp-PC.VMAPPS.COM>
2019-01-24 15:02:48 +0530

    new commited

--- python_program/Q2.py
+++ python_program/Q2.py
@@ -3,9 +3,9 @@ Write a program which will find all such numbers which are divisible by 7 but ar
 between 2000 and 3200 (both included).
 The numbers obtained should be printed in a comma-separated sequence on a single line."""
 
-output=[];
-for i in range(2000,3201) :
-	if (i%7 == 0) and (i%5 !=0):
-		output.append(i)
+output = []
+for i in range(2000, 3201):
+    if (i % 7 == 0) and (i % 5 != 0):
+        output.append(i)
 
 print(output)
--- python_program/class.py
+++ python_program/class.py
@@ -1,42 +1,44 @@
-
 """
 class TestClass:
 
-	#constructor start
-	def __init__(self):
-		print("In constructor")
-		#rettun 5 #constructor can't return anything
-		#constructor End
-	def testFunction1(self):
-		print("In test function")
+    # constructor start
+    def __init__(self):
+        print("In constructor")
 
-	def testFunction2(self):
-		print("in test Function2")
+    # rettun 5 #constructor can't return anything
+    # constructor End
+    def testFunction1(self):
+        print("In test function")
+
+    def testFunction2(self):
+        print("in test Function2")
+
 
 obj = TestClass()
 obj.testFunction1()
 obj.testFunction2()
 """
 
-#pivate and public
+# pivate and public
 
 class TestClass1(object):
-	"""docstring for TestClass1"""
+    """docstring for TestClass1"""
 
-	def __init__(self, arg):
-		self.arg = arg
+    def __init__(self, arg):
+        self.arg = arg
 
-	def __privateMethod(self):
-		return "increased by 1 : "
+    def __privateMethod(self):
+        return "increased by 1 : "
 
-	def publicMethod(self):
-		self.arg += 1
-		msg = self.__privateMethod()
-		#print(msg)
-		print(msg + str(self.arg))
+    def publicMethod(self):
+        self.arg += 1
+        msg = self.__privateMethod()
+        # print(msg)
+        print(msg + str(self.arg))
+
 
 obj1 = TestClass1(24)
 
 obj1.publicMethod()
-#access private method
+# access private method
 print obj1._TestClass1__privateMethod()
